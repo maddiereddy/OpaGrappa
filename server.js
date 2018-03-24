b9b261ddbf23e03d6296abe1bb46f7974aaf945b
maddiereddy <maddiereddy@gmail.com>
2018-03-24 15:13:51 -0700

    add a router

--- server.js
+++ server.js
@@ -1,15 +1,62 @@
 'use strict';
 
 const express = require('express');
-
+const morgan = require('morgan');
+const opaRouter = require('./router');
 const app = express();
 
+app.use(morgan('common'));
 app.use(express.static('public'));
 
-if (require.main === module) {
-  app.listen(process.env.PORT || 8080, function () {
-    console.info(`App listening on ${this.address().port}`);
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+// you need to import the router and route
+// requests to HTTP requests to `/` to `router`
+// app.use('/', opaRouter);
+
+// both runServer and closeServer need to access the same
+// server object, so we declare `server` here, and then when
+// runServer runs, it assigns a value.
+let server;
+
+// this function starts our server and returns a Promise.
+// In our test code, we need a way of asynchronously starting
+// our server, since we'll be dealing with promises there.
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log(`Your app is listening on port ${port}`);
+      resolve(server);
+    }).on('error', err => {
+      reject(err)
+    });
   });
 }
 
-module.exports = app;
+// like `runServer`, this function also needs to return a promise.
+// `server.close` does not return a promise on its own, so we manually
+// create one.
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => {
+      if (err) {
+        reject(err);
+        // so we don't also call `resolve()`
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
--- test/test-server.js
+++ test/test-server.js
@@ -2,18 +2,27 @@
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const app = require('../server.js');
+const {app, runServer, closeServer} = require('../server');
 
 const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-describe('index page', function () {
-  it('should exist', function () {
-    return chai.request(app)
-      .get('/')
-      .then(function (res) {
-        expect(res).to.have.status(200);
-      });
-  });
+describe('Opa Grappa', function () {
+  before(function() {
+		return runServer();
+	});
+
+	after(function() {
+		return closeServer();
+	});
+	
+	it('should return response 200 and html', function() {
+		return chai.request(app)
+			.get('/')
+			.then(function(res) {
+				res.should.have.status(200);
+				res.should.be.html;
+			})
+	});
 });
