f4376f422973d1093da39cdbc336228572e1a6a6
maddiereddy <maddiereddy@gmail.com>
2018-03-26 19:22:46 -0700

    add test for new pages

--- test/test-server.js
+++ test/test-server.js
@@ -17,9 +17,36 @@ describe('Opa Grappa', function () {
 		return closeServer();
 	});
 
-	it('should return response 200 and html', function() {
+	it('should return response 200 and index.html', function() {
 		return chai.request(app)
 			.get('/')
+			.then(function(res) {
+				expect(res).to.have.status(200);
+				expect(res).to.be.html;
+			})
+	});
+
+	it('should return response 200 and signup.html', function() {
+		return chai.request(app)
+			.get('/signup')
+			.then(function(res) {
+				expect(res).to.have.status(200);
+				expect(res).to.be.html;
+			})
+	});
+
+	it('should return response 200 and login.html', function() {
+		return chai.request(app)
+			.get('/login')
+			.then(function(res) {
+				expect(res).to.have.status(200);
+				expect(res).to.be.html;
+			})
+	});
+
+	it('should return response 200 and dashboard.html', function() {
+		return chai.request(app)
+			.get('/dashboard')
 			.then(function(res) {
 				expect(res).to.have.status(200);
 				expect(res).to.be.html;
