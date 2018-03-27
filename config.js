237bf541b03680a5bbbeaa3e8b47f4f08ab81327
maddiereddy <maddiereddy@gmail.com>
2018-03-27 02:23:39 -0700

    modify config

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/test';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test';
 exports.PORT = process.env.PORT || 8080;
-exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_SECRET = process.env.JWT_SECRET || 'super dooper trooper';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
